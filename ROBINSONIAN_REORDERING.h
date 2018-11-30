ec9c2ce172a9b5306518123609e0c379149f23ae
Sushranth Hebbar <sushranthhebbar@gmail.com>
2018-11-30 16:54:06 +0530

    Modified file reading

--- ROBINSONIAN_REORDERING.h
+++ ROBINSONIAN_REORDERING.h
@@ -15,7 +15,50 @@ private:
   void solve(vector<vector<double>> &arr,int size);
   void call_solve();
 public:
-  Bipolarization(string filename) : Square(filename) {}
+  Bipolarization(string filename) : Square(filename)
+  {
+    ifstream in( filename );
+    string line;
+    int countline = 0;
+    vector<vector<double> > matrix;
+      //_output = Matrix(get_rows(), get_rows());
+    while (getline( in, line ) )                   // read a whole line of the file
+    {
+        stringstream ss( line );                     // put it in a stringstream (internal stream)
+        vector<double> row;
+        string data;
+        if(countline==0) // The first row is filled with names
+        {
+            countline = 1;
+            while ( getline( ss, data, ',' ) )
+            {
+              //  _names.push_back(data);
+            }
+            continue;
+        }
+        if(countline==1)
+        {
+          countline=2;
+          while ( getline( ss, data, ',' ) )
+          {
+              //row.push_back(stod(data));
+          }
+          row.clear();
+        }
+        while ( getline( ss, data, ',' ) )           // read (string) items up to a comma
+        {
+            //cout<<data<<endl;
+            if(data=="eof") goto jump;
+            row.push_back(stod(data));            // use stod() to convert to double; put in row vector
+        }
+        if ( row.size() > 0 )  matrix.push_back( row );    // add non-empty rows to matrix
+    }
+    jump:
+    setMatrix(matrix);
+    set_rows(matrix.size());
+    set_columns( matrix[0].size());
+    //for(int i=0;i<_n;i++) _permutation.push_back(i);
+    }
   void compute();
 };
 #endif
